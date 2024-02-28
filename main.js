1031b88d6d69aeeadcff90eea376fa42b22f5588
nalisherovvna <mushknasiba@gmail.com>
2024-02-28 10:37:38 +0200

    it was my homework

---
+++ main.js
@@ -0,0 +1,10 @@
+let age =prompt("age= ")
+
+let name =prompt("name= ")
+
+let surname =prompt("surname= ")
+
+let adress =prompt("adress= ")
+
+let fav_subject =prompt("fav subject= ")
+alert ("my age =" +age +"\n my name = " + name +"\n my surname = " + surname +"\n my adress = " + adress +"\n my fav subject = " + fav_subject)
